10a989581cec3178c279e7f0133cfc7585b1a300
lndsilva <lndsilva@hotmail.com>
2022-08-16 21:49:57 -0300

    criando lista no fragment

--- app/src/main/java/br/com/myskout/tablayoutfragment/HomeFragment.java
+++ app/src/main/java/br/com/myskout/tablayoutfragment/HomeFragment.java
@@ -8,11 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class HomeFragment extends Fragment {
     ListView lstGatos;
+
+    //Criar os dados
+    String nomeGato[] = {"Panqueca", "Soneca", "Juma", "Ramela"};
+    String descricaoGato[] = {
+            "Quero ser adotado alguém ai se disponibiliza...",
+            "Alguém gosta de gato, estou aqui...",
+            "Gosto de humano, sou muito carinhoso...",
+            "Estou disponível para ser adotado..."
+
+    };
+    String numGato[] = {"5.0", "8.0", "9.0", "9.5"};
+    int imagemGato[] = {
+            R.drawable.gato, R.drawable.ciames,
+            R.drawable.rajado, R.drawable.preto_branco
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,12 +48,13 @@ public class HomeFragment extends Fragment {
 
         return view;
     }
+
     //Criando a minha classe adaptadora ou inner class
-    public class AdapterGatos extends BaseAdapter{
+    public class AdapterGatos extends BaseAdapter {
 
         @Override
         public int getCount() {
-            return 0;
+            return imagemGato.length;
         }
 
         @Override
@@ -51,7 +69,25 @@ public class HomeFragment extends Fragment {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            //Declarando as variáveis do modelo
+            TextView txtNomeGato, txtDescricaoGato, txtNumGato;
+            ImageView imgGato;
+
+            //Instanciar a janela do modelo no adapter
+            View v = getLayoutInflater().inflate(R.layout.modelo_gatos,null);
+
+            txtNomeGato = v.findViewById(R.id.txtNomeGato);
+            txtDescricaoGato = v.findViewById(R.id.txtDescricaoGato);
+            txtNumGato = v.findViewById(R.id.txtNumGato);
+            imgGato = v.findViewById(R.id.imgModeloGato);
+
+            //Inserindo valores nos objetos do modelo
+            txtNomeGato.setText(nomeGato[i]);
+            txtDescricaoGato.setText(descricaoGato[i]);
+            txtNumGato.setText(numGato[i]);
+            imgGato.setImageResource(imagemGato[i]);
+
+            return v;
         }
     }
 }
